789d63d46637b60286bbace11ab4d8d237ff7878
gweler <114002516+gweler@users.noreply.github.com>
2023-02-17 21:24:38 +0100

    Датотека креирана

---
+++ 01 Petlje/05-for.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int broj;
+    int najmanji_broj = 1000000;
+    int najveci_broj = 0;
+    int razlika;
+
+    for (;;) {
+
+        cout << "Unesite broj: ";
+        cin >> broj;
+
+        if (broj == 0) {
+            break;
+        } else if (broj > najveci_broj) {
+            najveci_broj = broj;
+        } else if (broj < najmanji_broj) {
+            najmanji_broj = broj;
+        }
+    }
+
+    cout << endl;
+    cout << "Najveci broj: " << najveci_broj <<endl;
+    cout << "Najmanji broj: " << najmanji_broj <<endl;
+    razlika = najveci_broj - najmanji_broj;
+    cout << "Razlika: " << razlika <<endl;
+
+    return 0;
+}
