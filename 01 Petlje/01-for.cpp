391c69f2b8e9f9c1446f29cc2953ccee10ce90b0
gweler <114002516+gweler@users.noreply.github.com>
2023-02-17 22:13:28 +0100

    Датотека креирана

---
+++ 01 Petlje/01-for.cpp
@@ -0,0 +1,33 @@
+#include <iomanip>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    float prosjek;
+    float zbir_ocjena = 0.0;
+    int ocjena = -1;
+    int broj_ocjena = 0;
+
+    for (;;) {
+        cout << "Unesite ocjenu: ";
+        cin >> ocjena;
+        if (ocjena == 0) {
+            break;
+        } else {
+            zbir_ocjena = zbir_ocjena + ocjena;
+            broj_ocjena = broj_ocjena + 1;
+        }
+    }
+
+    prosjek = zbir_ocjena / broj_ocjena;
+
+    cout << endl;
+    cout << "Prosjek unesenih ocjena je: ";
+    cout << fixed << setprecision(2);
+    cout << prosjek;
+    cout << endl;
+
+    return 0;
+}
