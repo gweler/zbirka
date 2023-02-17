8b1dd44c1a866e2ffcdb5821e107051dee4751d8
gweler <114002516+gweler@users.noreply.github.com>
2023-02-17 19:24:29 +0100

    Датотека креирана

---
+++ 01 Petlje/04-while.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int broj = -1;
+    int najmanji_broj = 1000000;
+
+    while (broj != 0) {
+
+        cout << "Unesite broj: ";
+        cin >> broj;
+
+        if (broj !=0 && broj < najmanji_broj) {
+            najmanji_broj = broj;
+        }
+    }
+
+    cout << endl;
+    cout << "Najmanji uneseni broj: " << najmanji_broj <<endl;
+
+    return 0;
+}
